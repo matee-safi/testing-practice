92af0ef6159b8f0c6452ca99ce54215d825f2e9b
Matee <matiullahsafibbc@gmail.com>
2023-03-09 14:55:24 -0600

    Complete chellenges

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,15 @@ const capitalize = require('./capitalize');
 test('Capitalize first letter', () => {
     expect(capitalize('chess')).toBe('Chess');
 })
+
+test('Capitalize first letter', () => {
+    expect(capitalize('4hess')).toBe('4hess');
+})
+
+test('Capitalize first letter', () => {
+    expect(capitalize('/hess')).toBe('/hess');
+})
+
+test('Capitalize first letter', () => {
+    expect(capitalize('Chess')).toBe('Chess');
+})
